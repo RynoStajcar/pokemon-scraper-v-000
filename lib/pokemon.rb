e7fc53ff38aa8150d2ba245bbc2d93f856c9301e
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-06 21:09:28 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,13 +3,6 @@ class Pokemon
   attr_accessor :id, :name, :hp, :type, :db
 
 
-  def initialize id:, name:, type:, hp: 60, db:
-    @id = id
-    @name = name
-    @type = type
-    @hp = hp
-    @db = db
-  end
 
   def self.save(name, type, db)
     db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
@@ -20,6 +13,14 @@ class Pokemon
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
+  def initialize id:, name:, type:, hp: 60, db:
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+  end
+  
   def alter_hp(new_hp, db)
     db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
   end
