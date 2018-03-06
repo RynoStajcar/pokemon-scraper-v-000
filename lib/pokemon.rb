d8c59aa0df68f2fcd502deaf44958d899e772879
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-06 21:13:59 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -13,7 +13,7 @@ class Pokemon
     Pokemon.new(id: pokemon_info[0], name: pokemon_info[1], type: pokemon_info[2], hp: pokemon_info[3], db: db)
   end
 
-  def initialize id:, name:, type:, hp:, db:
+  def initialize id:, name:, type:, hp: 60, db:
     @id = id
     @name = name
     @type = type
