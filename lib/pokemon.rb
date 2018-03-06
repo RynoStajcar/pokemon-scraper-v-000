1b578e374c10416b0d09dff278ca508d1eba2db4
Ryne Stajcar <rynostajcar@gmail.com>
2018-03-06 21:21:54 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,7 +2,7 @@ class Pokemon
 
   attr_accessor :id, :name, :hp, :type, :db
 
-  def initialize id:, name:, type:, hp: 60, db:
+  def initialize id:, name:, type:, hp: nil, db:
     @id = id
     @name = name
     @type = type
